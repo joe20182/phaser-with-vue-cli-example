c010f5dea57a1556f50819dd9d3c5efcdd00d193
joe.lin <joe.lin@silkrode.com.tw>
2019-12-05 17:52:10 +0800

    add sass and delete unused file

---
+++ src/views/Game.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="game-page">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Game',
+  data() {
+    return {
+      msg: 'test'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#game-page {
+  background-color: #eee;
+}
+</style>
